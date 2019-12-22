6797f907ad8723583769cf19193b7878b2f739cc
Christopher Pickering <Christopher.Pickering@pm.me>
2019-12-22 14:34:12 -0600

    updated commit

--- darby/test.jsx
+++ darby/test.jsx
@@ -5,5 +5,5 @@ $.writeln(os)
 
 var date = new Date();
 
-dateString = date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear() + '-' + date.getHours()+ '-' + date.getMinutes()
+dateString = date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear() + '-at-' + date.getHours()+ '-' + date.getMinutes()
 $.writeln(dateString)
